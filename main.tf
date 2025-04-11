585c9492fd41d8dea3272f5a596e5a0e841997d4
Abdul Rafay Modi <abdul.rafay@puresquare.com>
2025-04-11 22:28:25 +0500

    - Add tabix UI

--- main.tf
+++ main.tf
@@ -19,8 +19,8 @@ resource "docker_image" "clickhouse_server" {
 }
 
 resource "docker_container" "clickhouse_server" {
-  image = docker_image.clickhouse_server.image_id
-  name  = "clickhouse-server-tutorial"
+  image        = docker_image.clickhouse_server.image_id
+  name         = "clickhouse-server-tutorial"
   network_mode = docker_network.clickhouse_network.name
 
   ports {
@@ -42,6 +42,24 @@ resource "docker_container" "clickhouse_server" {
   volumes {
     container_path = "/var/lib/clickhouse"
     host_path      = "/click-house/"
+  }
+
+  restart = "unless-stopped"
+}
+
+resource "docker_image" "tabix" {
+  name         = "spoonest/clickhouse-tabix-web-client:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "tabix" {
+  image        = docker_image.tabix.image_id
+  name         = "tabix"
+  network_mode = docker_network.clickhouse_network.name
+
+  ports {
+    internal = 80
+    external = 8080
   }
 
   restart = "unless-stopped"
